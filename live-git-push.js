89591a9da3050a2eb817bf0435d0656e5f57a8ef
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2018-12-14 18:51:58 -0200

    Changes

--- live-git-push.js
+++ live-git-push.js
@@ -21,7 +21,6 @@ async function checkSync() {
 
             console.log("");
             console.log("Syncing...");
-            console.log("");
             await simpleGit.add('.');
             await simpleGit.commit("Changes");
             let r = await simpleGit.push('origin', 'master');
@@ -35,37 +34,49 @@ async function checkSync() {
     }
 }
 
-async function main() {
-    checkGitRepo();
-    checkSync();
+function startWathc() {
+
+    // Start watcher
+    let watcher = chokidar.watch('./', {
+        ignored: /(^|[\/\\])\../,
+        persistent: true,
+        ignoreInitial: true
+    });
+
+
+    let log = console.log.bind(console);
+
+    // Add event listeners
+    watcher
+        .on('add', path => notify(`File ${path} has been added`))
+        .on('change', path => notify(`File ${path} has been changed`))
+        .on('unlink', path => notify(`File ${path} has been removed`))
+        .on('addDir', path => notify(`Directory ${path} has been added`))
+        .on('unlinkDir', path => notify(`Directory ${path} has been removed`))
+        .on('error', error => log(`Watcher error: ${error}`))
+        .on('change', (path, stats) => {
+            if (stats) console.log(`File ${path} changed size to ${stats.size}`);
+        })
+        // .on('raw', (event, path, details) => {
+        //     log('Raw ev ent info:', event, path, details);
+        // })
+        .on('ready', () => {
+            log('Initial scan complete. Ready for changes');
+            checkGitRepo();
+            ready = true;
+        });
 }
 
-main();
+async function sync() {
+    await checkGitRepo();
+    await checkSync();
+}
 
-return;
-
-program.version(pkg.version);
-program.parse(process.argv);
-
-// Start watcher
-let watcher = chokidar.watch('./', {
-    ignored: /(^|[\/\\])\../,
-    persistent: true,
-    ignoreInitial: true
-});
-
-
-let run = function (command) {
-    return new Promise(function (resolve, reject) {
-        exec(command, (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(stdout, stderr);
-            }
-        });
-    });
-};
+async function main() {
+    // First Sync
+    await sync();
+   // startWathc();
+}
 
 async function checkGitRepo() {
     hasGitRepo = await simpleGit.checkIsRepo();
@@ -74,60 +85,60 @@ async function checkGitRepo() {
     return hasGitRepo;
 }
 
-let notify = function (value) {
-    console.log(value);
-    if (!ready)
-        return;
 
-    // Check for git repo
-    if (!hasGitRepo && !checkGitRepo())
-        return;
+main();
 
-    console.log("");
-    console.log("Syncing...");
-    run('git add .').then(
-        (stdout, stderr) => {
-            console.log(stdout);
-            run('git commit -m "Changes"').then(
-                (stdout, stderr) => {
-                    console.log(stdout);
-                    run('git push origin master').then(
-                        (stdout, stderr) => {
-                            console.log(stdout);
-                            console.log("");
-                            console.log("Sync finished");
-                        }
-                    )
-                }
-            )
-        }).catch((err) => {
-        console.log("Error running");
-        console.error(err);
-    });
-};
+return;
 
-let log = console.log.bind(console);
+// program.version(pkg.version);
+// program.parse(process.argv);
 
-// Add event listeners.
-watcher
-    .on('add', path => notify(`File ${path} has been added`))
-    .on('change', path => notify(`File ${path} has been changed`))
-    .on('unlink', path => notify(`File ${path} has been removed`))
-    .on('addDir', path => notify(`Directory ${path} has been added`))
-    .on('unlinkDir', path => notify(`Directory ${path} has been removed`))
-    .on('error', error => log(`Watcher error: ${error}`))
-    .on('change', (path, stats) => {
-        if (stats) console.log(`File ${path} changed size to ${stats.size}`);
-    })
-    // .on('raw', (event, path, details) => {
-    //     log('Raw ev ent info:', event, path, details);
-    // })
-    .on('ready', () => {
-        log('Initial scan complete. Ready for changes');
-        checkGitRepo();
-        ready = true;
-    });
+//
+// let run = function (command) {
+//     return new Promise(function (resolve, reject) {
+//         exec(command, (err, stdout, stderr) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(stdout, stderr);
+//             }
+//         });
+//     });
+// };
 
+//
+// let notify = function (value) {
+//     console.log(value);
+//     if (!ready)
+//         return;
+//
+//     // Check for git repo
+//     if (!hasGitRepo && !checkGitRepo())
+//         return;
+//
+//     console.log("");
+//     console.log("Syncing...");
+//     run('git add .').then(
+//         (stdout, stderr) => {
+//             console.log(stdout);
+//             run('git commit -m "Changes"').then(
+//                 (stdout, stderr) => {
+//                     console.log(stdout);
+//                     run('git push origin master').then(
+//                         (stdout, stderr) => {
+//                             console.log(stdout);
+//                             console.log("");
+//                             console.log("Sync finished");
+//                         }
+//                     )
+//                 }
+//             )
+//         }).catch((err) => {
+//         console.log("Error running");
+//         console.error(err);
+//     });
+// };
+//
 
 // let watchedPaths = watcher.getWatched();
 
